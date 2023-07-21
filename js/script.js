07867ac10acfa4629e29c9760a6c27f811e718cf
Junasukses <xyaruna@gmail.com>
2023-07-21 10:39:11 +0700

    refactor: navbar onscroll change color

--- js/script.js
+++ js/script.js
@@ -1,10 +1,21 @@
+const firstLogo = "./images/logo/QL_Logo_white.png";
+const secondLogo = "./images/logo/QL_Logo_2.png";
+
 const nav = document.getElementById("nav");
+const ulNav = document.getElementById("ulNav");
+const logo = document.getElementById("logo-ql");
 window.onscroll = function () {
 	if (this.scrollY >= 20) {
-		nav.classList.add("backdrop-blur-[14px]");
+		logo.src=secondLogo;
+		nav.classList.add("bg-white");
+		ulNav.classList.add("text-[#272643]");
 		nav.classList.remove("bg-transparent");
+		ulNav.classList.remove("text-white");
 	} else {
+		logo.src=firstLogo;
 		nav.classList.add("bg-transparent");
-		nav.classList.remove("backdrop-blur-[14px]");
+		ulNav.classList.add("text-white");
+		nav.classList.remove("bg-white");
+		ulNav.classList.remove("text-[#272643]");
 	}
 };
